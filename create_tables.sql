0e4881714e7334ec006dd241ad4f7677762ade6e
Doug McGeehan <djmvfb@mst.edu>
2017-11-13 07:41:53 -0600

    cleaned up table creation settings

--- create_tables.sql
+++ create_tables.sql
@@ -3,13 +3,8 @@ SET standard_conforming_strings = off;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
-
-SET search_path = discogs;
-
 SET default_tablespace = '';
-
 SET default_with_oids = false;
-
 SET synchronous_commit=off;
  
 CREATE UNLOGGED TABLE artist (
@@ -201,16 +196,3 @@ CREATE UNLOGGED TABLE masters_images (
     image_uri text
 );
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
-SET search_path = discogs;
-
-SET default_tablespace = '';
